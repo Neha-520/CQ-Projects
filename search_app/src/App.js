0a089f1cc8274cf6ca7688d20e73e0a1c8e9b79c
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-09-19 20:30:19 +0530

    final commit

--- search_app/src/App.js
+++ search_app/src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data.json'
 import './App.css';
 
 function App() {
+  const [state, setstate] = useState("");
+
   return (
     <div className="App container">
-      <input type="text" placeholder="Search Your Restaurants"></input>
-      <table>
-        <thead>Name</thead>
-        <thead>Location</thead>
+      <input type="text" name="name" onChange={(e) => setstate(e.target.value)} placeholder="Search Your Restaurants"></input>
+      <table style={{ width: "100%", marginTop: "1rem" }}>
+        <tr style={{ backgroundColor: "white", textAlign: "left" }}>
+          <th className="a" colspan="1">Name</th>
+          <th  >Location</th>
+        </tr>
+        {
+          data.employee.filter((val) => {
+            if (state === "") return val;
+            else if (val.name.toLowerCase().includes(state.toLowerCase()))
+              return val;
+            return false;
+          }).map((val) => {
+            return (
+              <tr>
+                <td >{val.name}</td>
+                <td>{val.location}</td>
+              </tr>
+            );
+          })
+        }
       </table>
     </div>
   );
