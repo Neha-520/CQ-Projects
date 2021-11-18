dbd94ff2ebd7ba70b099ccb01ff3525292f364d4
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-11-18 15:01:35 +0530

    login route setup

--- ECommerce/main.js
+++ ECommerce/main.js
@@ -1,5 +1,28 @@
 const express = require("express");
 const fs = require("fs");
+const session = require("express-session")
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.render("landingPage");
+})
+
+app.route('/login').get(function (req, res) {
+    res.render("login", { error: "" });
+})
+    .post(function (req, res) {
+        const { username, password } = req.body;
+    });
+
+app.route('/signup').get(function (req, res) {
+    res.render("signup");
+})
+    .post(function (req, res) {
+
+    });
